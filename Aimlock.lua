19e6d3ae0282c7e38913e5af3ec77b6d2facb1a2
EITAPORRA2 <135403977+EITAPORRA2@users.noreply.github.com>
2023-09-06 20:41:23 -0300

    Update Aimlock.lua

--- Aimlock.lua
+++ Aimlock.lua
@@ -1,14 +1,3 @@
---[[
-getgenv().AimPart = "HumanoidRootPart" -- For R15 Games: {UpperTorso, LowerTorso, HumanoidRootPart, Head} | For R6 Games: {Head, Torso, HumanoidRootPart}
-getgenv().AimlockToggleKey = "Y" -- Toggles Aimbot On/Off 
-getgenv().AimRadius = 50 -- How far away from someones character you want to lock on at
-getgenv().ThirdPerson = false -- Locking onto someone in your Third Person POV
-getgenv().FirstPerson = true -- Locking onto someone in your First Person POV
-getgenv().TeamCheck = false -- Check if Target is on your Team (True means it wont lock onto your teamates, false is vice versa) (Set it to false if there are no teams)
-getgenv().PredictMovement = true -- Predicts if they are moving in fast velocity (like jumping) so the aimbot will go a bit faster to match their speed 
-getgenv().PredictionVelocity = 10 -- The speed of the PredictMovement feature 
-]]--
-
 getgenv().UniverseHubLoadingTime = tick()
 if UniverseHubLoadingTime == true then
     Notify("Universe Hub", "Script Loaded Already", "", 3)
@@ -36,7 +25,7 @@ getgenv().Notify = function(title, text, icon, time)
         SGui:SetCore("SendNotification",{
             Title = title;
             Text = text;
-            Icon = "rbxassetid://5793181157";
+            Icon = "";
             Duration = time;
         }) 
     end
@@ -222,4 +211,4 @@ RService.RenderStepped:Connect(function()
     end
 end)
 
-SeparateNotify("Universe Hub", "Universal Aimbot loaded in: "..string.format("%.7f", tostring(tick() - UniverseHubLoadingTime)), "", 3)
+SeparateNotify("Ciazware", "Universal Aimbot loaded in: "..string.format("%.7f", tostring(tick() - UniverseHubLoadingTime)), "", 3)
